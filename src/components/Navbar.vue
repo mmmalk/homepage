40073e1779370fafc6bc0e617992bc4e8eb11ef1
Mikko Malkavaara <mikko.malkkis@gmail.com>
2020-12-30 01:12:21 +0200

    Finished About

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,40 +1,47 @@
 <template>
-<div>
-  <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
-    <b-navbar-brand>
-      <img src="../../public/layermaskpepper.png" class="brand">
-    </b-navbar-brand>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
+      <b-navbar-brand>
+        <img src="../../public/layermaskpepper.png" class="brand" />
+      </b-navbar-brand>
       <b-navbar-nav>
-        <b-nav-item :to="{name:'Home'}">Home</b-nav-item>
-        <b-nav-item :to="{name: 'About'}">About me</b-nav-item>
+        <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
+        <b-nav-item :to="{ name: 'About' }">About me</b-nav-item>
         <b-nav-item href="#">CV</b-nav-item>
         <b-nav-item href="#">Contacts</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-text>Mikko "malkkis" Malkavaara</b-nav-text>
-        <b-nav-item href="https://www.linkedin.com/in/mikko-malkavaara-856265156/">
-          <img src="../../public/iconmonstr-linkedin-3.svg" class="graphic-dark">
+        <b-nav-item
+          href="https://www.linkedin.com/in/mikko-malkavaara-856265156/"
+        >
+          <img
+            src="../../public/iconmonstr-linkedin-3.svg"
+            class="graphic-dark"
+          />
         </b-nav-item>
         <b-nav-item href="https://github.com/mmmalk/">
-          <img src="../../public/iconmonstr-github-1.svg" class="graphic-dark">
+          <img
+            src="../../public/iconmonstr-github-1.svg"
+            class="graphic-dark"
+          />
         </b-nav-item>
       </b-navbar-nav>
-  </b-navbar>
-</div>
+    </b-navbar>
+  </div>
 </template>
 
 <style>
 .graphic-dark {
-  filter: invert(65%)
+  filter: invert(65%);
 }
 .brand {
   width: 40%;
-
 }
 </style>
 
 <script>
 export default {
-  name: "Navbar"
-}
+  name: "Navbar",
+};
 </script>
