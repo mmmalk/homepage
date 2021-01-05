d144cc866bd339c6d982ca12d430f30753541b66
Mikko Malkavaara <mikko.malkkis@gmail.com>
2021-01-05 17:50:49 +0200

    http redirect to https, new paragraph to home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,6 +5,10 @@
       My name is Mikko Malkavaara, and this is my website made with
       <a href="https://vuejs.org/">Vue.js</a>
     </p>
+    <p>
+      This site is still bit unfinished, but you can view the source code
+      <a href="https://github.com/mmmalk/homepage"> here</a>
+    </p>
   </div>
 </template>
 
